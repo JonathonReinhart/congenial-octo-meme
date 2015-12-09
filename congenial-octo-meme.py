c26b4401dfced530a5ae9e3cc7b6b1185e26b288
Jonathon Reinhart <jreinhart@cc-sw.com>
2015-12-09 10:09:45 -0500

    complete

---
+++ congenial-octo-meme.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+# https://twitter.com/OldManKris/status/673184195485790208
+# http://imgur.com/Quyu1Pc
+
+def main():
+    print("            Hey girl")
+    print("           *        *")
+    print("       is your        name")
+    print("        *              *")
+    print("Google?                Cause you've")
+    print("       *               *")
+    print("       *               *")
+    print("     got               everything")
+    print("        *              *")
+    print("         I'm        searching")
+    print("           *        *")
+    print("             **for**")
+
+if __name__ == '__main__':
+    main()
